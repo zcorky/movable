ea088395176463ee923b6d69bba9e8b062ff494f
tobewhatwewant <tobewhatwewant@gmail.com>
2018-11-16 15:35:58 +0800

    :bug:(CI): fix travis-ci deploy config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 echo "deploy to gh-pages."
 
-cp -rf lib example
-cd example
+cp -rf lib gh-pages
+cp -rf example/* gh-pages
+cd gh-pages
 git add --all .
 git commit -a -m ":sparkles:(TravisCI) automatically update from travis_ci (version: $TRAVIS_TAG)"
 git push --quiet "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" gh-pages:gh-pages
