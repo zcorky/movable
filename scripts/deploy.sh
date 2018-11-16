47c4780c93cd766436da843ea3e672b700ba2418
tobewhatwewant <tobewhatwewant@gmail.com>
2018-11-16 15:16:26 +0800

    :bug:(deploy): fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 echo "deploy to gh-pages."
 
-rm -rf gh-pages/*
-cp -rf dist/* gh-pages
-cp CNAME gh-pages
-cd gh-pages
+cp -rf lib example
+cd example
 git add --all .
 git commit -a -m ":sparkles:(TravisCI) automatically update from travis_ci (version: $TRAVIS_TAG)"
 git push --quiet "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git" gh-pages:gh-pages
